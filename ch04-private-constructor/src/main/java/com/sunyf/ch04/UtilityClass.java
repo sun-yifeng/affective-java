dcea71fd571fb44169dd534e545ad3ad82de7d1f
sunyifeng <9339996@qq.com>
2018-11-01 20:37:07 +0800

    utility class

---
+++ ch04-private-constructor/src/main/java/com/sunyf/ch04/UtilityClass.java
@@ -0,0 +1,14 @@
+package com.sunyf.ch04;
+
+/**
+ * @program: affective-java
+ * @description: 通过私有构造方法避免实例化-工具类
+ * @author: Mr.Sun
+ * @create: 2018-11-01 20:32
+ **/
+public class UtilityClass {
+
+    private UtilityClass() {}
+
+    // do sth
+}
